6082def244707ed145904de273a75128dbecaac5
djcerdas <d2words@gmail.com>
2017-12-29 12:34:51 -0600

    Add AUTHOR, LICENSE and DESCRIPTION  to the module
    Add AUTHOR, LICENSE and DESCRIPTION  to the module

--- ByLibrary/module.h/helloWorld.c
+++ ByLibrary/module.h/helloWorld.c
@@ -1,6 +1,10 @@
 #include <linux/module.h>       /* Needed by all modules */
 #include <linux/kernel.h>       /* Needed for KERN_INFO */
 
+MODULE_AUTHOR ("X Men");
+MODULE_LICENSE("GPLv2"); 
+MODULE_DESCRIPTION ("A very simple module");
+
 int init_module(void)
 {
         printk(KERN_INFO "Hello world!\n");
